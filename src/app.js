3158a8037253b55ed88029ed4bf8d75951573669
cg-diazborromeo <Gastadiaz@gmail.com>
2022-09-22 01:19:05 -0300

    wip: Agrega imagen de logo y botones de paginacion

--- src/app.js
+++ src/app.js
@@ -1,30 +1,65 @@
 const baseURL = new URL ('https://pokeapi.co/api/v2');
 const $listaPokemones = document.querySelector('#lista-pokemones');
+const $listaSiguiente = document.querySelector('#siguiente');
+const $listaPrevia = document.querySelector('#previo');
+let pokemonInicio = 1;
+let pokemonFinal = 18;
 let arrayPokemones = [];
 
-obtenerPokemones();
+obtenerPokemones(pokemonInicio, pokemonFinal);
+
+$listaSiguiente.onclick = function(event) {
+    resetearPokemones();
+    pokemonInicio += 18;
+    pokemonFinal += 18;
+    obtenerPokemones();
+
+    event.preventDefault(pokemonInicio, pokemonFinal);
+}
+
+$listaPrevia.onclick = function(event) {
+    resetearPokemones();
+    pokemonInicio -= 18;
+    pokemonFinal -= 18;
+    obtenerPokemones(pokemonInicio, pokemonFinal);
+    
+    event.preventDefault();
+}
+
 
 function obtenerPokemones() {
-    for (let i = 1; i <= 18; i++) {
+    for (let i = pokemonInicio; i <= pokemonFinal; i++) {
         fetch(`${baseURL}/pokemon-form/${i}`)
         .then(respuesta => respuesta.json())
         .then(respuesta => {
             const $imgPokemon = document.createElement('img');
-            const $namePokemon = document.createElement('p');
-            const $Container = document.createElement('div');
+            const $namePokemon = document.createElement('a');
+            const $container = document.createElement('div');
+            const $nameContainer = document.createElement('p');
 
-            $Container.classList = "container text-center";
+            $container.classList = "container text-center";
             $imgPokemon.classList = 'col';
             $namePokemon.classList = 'col';
-            $imgPokemon.src = respuesta.sprites.front_default;
-            $namePokemon.textContent = respuesta.pokemon.name;
 
-            $Container.appendChild($imgPokemon);
-            $Container.appendChild($namePokemon);
-            $listaPokemones.appendChild($Container);
+            $imgPokemon.src = respuesta.sprites.front_default;
+            $namePokemon.textContent = (respuesta.pokemon.name).charAt(0).toUpperCase() + (respuesta.pokemon.name).slice(1);
+            $namePokemon.href = respuesta.pokemon.url;
+            $container.id = 'pokemon-container';
+
+            $nameContainer.appendChild($namePokemon);
+            $container.appendChild($imgPokemon);
+            $container.appendChild($nameContainer)
+            $listaPokemones.appendChild($container);
         })
         .catch(error => console.log('No se obtuvieron resultados', error));
     }
+}
+
+function resetearPokemones() {
+    const $imagenesSeleccionadas = document.querySelectorAll('#pokemon-container img');
+    $imagenesSeleccionadas.forEach(imagen => {
+        $listaCambios.removeChild(imagen);
+    });
 }
 
 // function obtenerNombres() {
